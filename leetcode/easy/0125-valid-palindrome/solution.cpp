f73fdb6998672f0dd0d4d13906fc082cff739150
Yashvardhan Sengar <yashvardhansengar200@gmail.com>
2026-09-06 16:57:05 +0530

    ✅ Valid Palindrome | Easy | C++ | Runtime: 0 ms | Memory: 7.7 MB | 2026-09-06

--- leetcode/easy/0125-valid-palindrome/solution.cpp
+++ leetcode/easy/0125-valid-palindrome/solution.cpp
@@ -3,7 +3,7 @@ public:
     bool isPalindrome(string s) {
         string str="", rev="";
         for(char ch:s){
-        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' || ch<='9')
         str=str+ch;}
         transform(str.begin(), str.end(), str.begin(), ::tolower);
         rev=str;
